484a30e167a6ba970a90e6551057dfd71a91b284
sethrimer3 <sethrimer3@gmail.com>
2026-06-04 09:48:35 -0600

    auto: index.ts

--- src/sim/index.ts
+++ src/sim/index.ts
@@ -1,4 +1,4 @@
-export { createGameState, tapEquation, tryPurchaseUpgrade, tryUnlockNextTier, tryUnlockEquationForge, tryUpgradeLoom, tryPurchaseSpecialLoom, tryAlivenMote, tryUpgradeLoomEfficiencyAction, tapEquationForge, processLoomCapture, applyForgeSacrifice, craftWeapon, craftWeave, simTick, getScore } from './game-state';
+export { createGameState, tapEquation, tryPurchaseUpgrade, tryUnlockNextTier, tryUnlockEquationForge, tryUpgradeLoom, tryPurchaseSpecialLoom, tryAlivenMote, tryUpgradeLoomEfficiencyAction, tapEquationForge, processLoomCapture, applyForgeSacrifice, craftWeapon, craftWeave, craftLens, attachLensToWeapon, simTick, getScore } from './game-state';
 export type { GameState, TapResult, SimTickResult } from './game-state';
 
 export * from './equation';
